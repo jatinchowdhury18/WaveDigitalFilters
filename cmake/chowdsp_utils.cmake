b1c21802590588fef33a39efcd7c271c5a74924f
Daniel Walz <developer@foleysfinest.com>
2024-03-04 17:19:15 +0100

    Removed submodules and use cmake FetchContent instead

---
+++ cmake/chowdsp_utils.cmake
@@ -0,0 +1,6 @@
+FetchContent_Declare(chowdsp_utils
+        GIT_REPOSITORY https://github.com/Chowdhury-DSP/chowdsp_utils
+        GIT_TAG main
+        GIT_SHALLOW ON)
+
+FetchContent_MakeAvailable(chowdsp_utils)
