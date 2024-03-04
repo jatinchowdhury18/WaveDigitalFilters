e48beb94612f5354c85ffddd338690831de88088
Daniel Walz <developer@foleysfinest.com>
2024-03-04 18:43:03 +0100

    Fixed inconsistent branch names

--- cmake/chowdsp_utils.cmake
+++ cmake/chowdsp_utils.cmake
@@ -1,6 +1,6 @@
 FetchContent_Declare(chowdsp_utils
-        GIT_REPOSITORY https://github.com/Chowdhury-DSP/chowdsp_utils
-        GIT_TAG main
+        GIT_REPOSITORY https://github.com/Chowdhury-DSP/chowdsp_utils.git
+        GIT_TAG master
         GIT_SHALLOW ON)
 
 FetchContent_MakeAvailable(chowdsp_utils)
--- cmake/chowdsp_wdf.cmake
+++ cmake/chowdsp_wdf.cmake
@@ -1,5 +1,5 @@
 FetchContent_Declare(chowdsp_wdf
-        GIT_REPOSITORY https://github.com/Chowdhury-DSP/chowdsp_wdf
+        GIT_REPOSITORY https://github.com/Chowdhury-DSP/chowdsp_wdf.git
         GIT_TAG main
         GIT_SHALLOW ON)
 
